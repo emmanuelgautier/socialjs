59c759ef0d3c1ef9980e89b0d63b3df899b16e46
emmanuelgautier <manu.gautier1394@gmail.com>
2013-10-22 22:58:13 +0200

    Bug fix : null data not included in url

--- social.js
+++ social.js
@@ -41,7 +41,7 @@
 
     _mergeData2Url  = function( url, data ){
         var params = '?';
-        for (var key in data) if(data[key]){
+        for (var key in data) if(data.hasOwnProperty(key)){
             params += encodeURIComponent(key) + '=' +
             encodeURIComponent(data[key]) + '&';
         }
