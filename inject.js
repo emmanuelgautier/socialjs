aaa62a87cf82b0ff55c17484a693d839b26ffcf6
manu <manu@manu-pc>
2014-02-21 20:33:41 +0100

    some chrome apps adaptation

--- inject.js
+++ inject.js
@@ -2,10 +2,11 @@ var url = window.location.href;
 var params = '?';
 var index = url.indexOf(params);
 if (index > -1) {
-  params = url.substring(index);
+    params = url.substring(index);
 }
 
 params += '&from=' + encodeURIComponent(url);
 
 var redirect = chrome.extension.getURL('socialjs/popup.html');
-window.location = redirect + params;
+
+window.location.href = redirect + params;
--- popup.js
+++ popup.js
@@ -1,8 +1,38 @@
-chrome.runtime.getBackgroundPage(function( opener ){
-    opener.social.setAuthUrl( window.location.href );
-    
-    setTimeout(function(){
-        window.open('','_self');
+/*chrome.runtime.getBackgroundPage(function(opener) {
+    opener.social.setAuthUrl(window.location.href);
+
+    setTimeout(function() {
+        window.open('', '_self');
         window.close();
     }, 500);
+});*/
+
+var view = document.createElement('webview');
+    view.style.width = "100%";
+    view.style.height = "100%";
+    view.src = document.location.href.match(/\?u=(.*)/)[1];
+
+document.body.appendChild(view);
+view.addEventListener('loadcommit', function(view){
+    if(!/https:\/\/(www\.google\.com\/robots\.txt#|www\.facebook\.com\/connect\/login_success\.html)(.*)/.test(view.url)){
+        return false;
+    }
+
+    chrome.runtime.getBackgroundPage(function(background){
+        background.social.windowResponse({
+            url: view.url,
+            content: null
+        });
+        window.close();
+    });
+
+    /*view.executeScript({code: 'document.body.innerHTML'}, function(content){
+        chrome.runtime.getBackgroundPage(function(background){
+            background.social.windowResponse({
+                url: view.url,
+                content: content
+            });
+            window.close();
+        });
+    });*/
 });
